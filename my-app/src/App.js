0cbbd86dcbb243da145b892be1391964dac464eb
AllaOz <ozcanalla@gmail.com>
2023-06-20 21:30:59 +0300

    test1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 
-import Header from './components/header';
-import Footer from './components/footer';
-import Body from './components/body'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body'
 
 function App() {
   return (
